52ff65dbb5055e018e1b090e915a66f97ad0fb44
yury_timohin <yury_timohin@mail.ru>
2022-12-16 20:08:48 +0300

    Написали программу для возведения числа в квадрат

--- LAB1/Program.cs
+++ LAB1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// тип переменной <пробел> имя переменной = конвертирование в тип данных переменной + считывание с консоли
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+// возвести число в квадрат
+int sqr = number * number;
+
+// возвести в квадрат с помощью класса Math
+int sqrMath = Convert.ToInt32(Math.Pow(number, 2));
+
+System.Console.WriteLine(sqrMath);

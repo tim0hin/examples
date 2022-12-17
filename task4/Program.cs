151f5698582ec614333ebb287b14641dd8f6283d
yury_timohin <yury_timohin@mail.ru>
2022-12-17 11:29:27 +0300

    Выполнил задачу 4: нахождение максимального из 3-х чисел

---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+System.Console.WriteLine("Введите 3 числа: ");
+
+int[] nums = new int[] {Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())};
+
+int max = nums[0];
+
+for (int i = 0; i < nums.Length-1; i++)
+{
+
+    if (nums[i] > max) max = nums[i];
+
+}
+
+System.Console.Write("Максимальное число: ");
+System.Console.WriteLine(max);

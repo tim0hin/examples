84b9e9eb0966bd6f60e8b6c5117f3a76b06d94dd
yury_timohin <yury_timohin@mail.ru>
2022-12-17 11:40:51 +0300

    Выполнил задачу 6: проверить число на четность.

---
+++ task6/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число и выдает,
+// является ли число четным (делится ли оно на два без остатка).
+
+System.Console.WriteLine("Введите число для проверки четности: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number % 2;
+
+if (result == 0)
+{
+    System.Console.WriteLine(number + " - четное число");
+}
+else
+{
+
+    System.Console.WriteLine(number + " - нечетное число");
+
+};

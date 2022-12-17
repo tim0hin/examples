a64ee83d8b112fd7fc46b20c7155a640b8a0b4b3
yury_timohin <yury_timohin@mail.ru>
2022-12-17 11:53:53 +0300

    Выполнил задачу 8: показать все четные числа от 1 до N

---
+++ task8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе
+// показывает все четные числа от 1 до N.
+
+System.Console.Write("Введите число N: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < N; i++)
+{
+
+    int parity = i % 2;
+
+    if (parity == 0) System.Console.Write(i + " ");
+
+};

b0c1634c36cd4342e71b034a214b25aa49d28c98
yury_timohin <yury_timohin@mail.ru>
2022-12-17 10:59:52 +0300

    Сделал задачу 2. Нахождение большего и меньшего из двух чисел

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на входе принимает два числа и выдает, какое число большее, а какое меньшее.
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int max,min;
+
+if (a<b)
+{
+
+    max = b;
+    min = a;
+
+}
+else
+{
+
+    max = a;
+    min = b;
+
+}
+
+System.Console.WriteLine(max + " - это число большее");
+System.Console.WriteLine(min + " - это число меньшее");

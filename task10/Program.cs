c0cee2db77a213c280110d2a855edd8110257327
yury_timohin <yury_timohin@mail.ru>
2022-12-21 22:30:03 +0300

    Выполнил задачу 10: написать программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+
+int a = new Random().Next(100,1000);
+System.Console.WriteLine(a);
+
+int secondDigit = a / 10 % 10;
+
+System.Console.WriteLine(secondDigit);
+
+

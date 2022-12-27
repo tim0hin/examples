a67e8d278905a3f60fc07d9b16285c8133bc9eda
yury_timohin <yury_timohin@mail.ru>
2022-12-27 22:25:29 +0300

    Выполнил задачу 19: написать программу для проверки числа на полиндром

---
+++ task19/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+bool Polindrom(int num)
+{
+    string numStr = Convert.ToString(num);
+    bool polindrom = false;
+
+    for (int i = 0; i < numStr.Length/2; i++)
+    {
+            if (numStr[i] == numStr[numStr.Length-1-i]) polindrom = true;
+            else polindrom = false;
+    }
+    return polindrom;
+}
+
+int number = 0;
+
+do
+{  
+    System.Console.Write("Введите пятизначное число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}while (Convert.ToString(number).Length != 5);
+
+System.Console.WriteLine($"This is polindrom - {Polindrom(number)}");

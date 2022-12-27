8dbc31cf8569d9b75975eb82d7e247e27bab79fc
yury_timohin <yury_timohin@mail.ru>
2022-12-27 23:27:55 +0300

    Выполнил задачу 23: вывести таблицу кубов от 1 до N

---
+++ task23/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int InputValue(string message)
+{
+    System.Console.Write(message);
+    
+    int value = Convert.ToInt32(Console.ReadLine());
+    
+    if (value > 0) return value;
+    else return InputValue(message);
+}
+
+string TableOfCubes(int N)
+{
+    string strCubes = string.Empty;
+    for (int i = 1; i <= N; i++)
+        if (i == N) strCubes+=Math.Pow(i,3);
+        else strCubes = strCubes + Math.Pow(i,3) + ", ";
+    return strCubes;
+}
+
+int N = InputValue("Введите число N: ");
+
+System.Console.WriteLine($"Таблица кубов числа N: \n {TableOfCubes(N)}");

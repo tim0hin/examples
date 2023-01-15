24fea922f01665e35b728edd08910ecaa8a3f654
yury_timohin <yury_timohin@mail.ru>
2023-01-15 18:43:38 +0300

    Выполнил задачу 29: Написать программу, которая задаёт массив из 8-ми элементов и выводит их на экран

---
+++ task29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] CreateArray()
+{
+    int[] array = new int[8];
+
+    for (int i = 0; i < array.Length; i++)
+    {  
+        array[i] = new Random().Next(1,100);
+    }
+
+    return array;
+}
+
+System.Console.WriteLine(string.Join(", ", CreateArray()));

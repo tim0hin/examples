af556f7e3d7ae87d55b67a8a131fe3afbb50e861
yury_timohin <yury_timohin@mail.ru>
2023-01-15 17:46:06 +0300

    Выполнил задачу 25: Возведение числа A в натуральную степень B

---
+++ task25/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+double InputNumber(string message)
+{
+    System.Console.Write(message);
+    
+    double number = Convert.ToInt32(Console.ReadLine());
+    
+    return number;
+}
+
+double DegreeOfNumber(double A, double B)
+{
+    double degree = 0;
+    
+    //double degree = Math.Pow(A, B);   //сначала было так, а потом увидел, что по условию нужен цикл.
+
+    for (int i = 0; i <= B; i++) degree = Math.Pow(A, i);
+
+    return degree;
+}
+
+double A = InputNumber($"Введите число А: ");
+double B = InputNumber($"Введите число B: ");
+
+System.Console.WriteLine($"Натуральная степень числа {A} = {DegreeOfNumber(A, B)}");

8517cf652c24bf7b6f8f03797ec14ca281b84098
yury_timohin <yury_timohin@mail.ru>
2023-01-18 20:55:29 +0300

    Выполнил задачу 36: Найти сумму элементов массива на нечетных позициях

---
+++ task36/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1,100);
+    }
+    return array;
+}
+
+int FindSummOfElementsOddIndex(int[] array)
+{
+    int summ = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 != 0) summ += array[i];
+    }
+    return summ;
+}
+
+int[] array = new int[4];
+FillArray(array);
+System.Console.WriteLine("Массив:");
+System.Console.WriteLine(String.Join(", ", array));
+System.Console.WriteLine($"Сумма элементов с нечетным индексом = {FindSummOfElementsOddIndex(array)}");

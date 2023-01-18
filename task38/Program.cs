fcbfd99698fb134cf10deffa8e17ee76fdf84989
yury_timohin <yury_timohin@mail.ru>
2023-01-18 21:17:35 +0300

    Выполнил задачу 38: Найти разницу между максимальным и минимальным элементом массива.

---
+++ task38/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int FindMinNumber(int[] array)
+{
+    int min = array[0];
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < min) min = array[i];
+    }
+    return min;
+}
+
+int FindMaxNumber(int[] array)
+{
+    int max = array[0];
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > max) max = array[i];
+    }
+    return max;
+}
+
+int DiffOfMinMax(int min, int max)
+{
+    int diff = max - min;
+    return diff;
+}
+
+int[] array = new int[5] {3, 7, 22, 2, 78};
+System.Console.WriteLine(String.Join(", ", array));
+// System.Console.WriteLine($"Минимальный элемент = {FindMinNumber(array)}");
+// System.Console.WriteLine($"Максимальный элемент = {FindMaxNumber(array)}");
+System.Console.Write("Разница между максимальным и минимальным элементом массива = ");
+System.Console.WriteLine(DiffOfMinMax(FindMinNumber(array),FindMaxNumber(array)));
